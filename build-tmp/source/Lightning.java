b4d3d1cffb85601f9af413b46184dbb16e0ba196
unknown <ivkwong1@697-334-Nogrod.sf.k12.internal>
2015-09-04 08:30:14 -0700

    More ZAP

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -1,15 +1,31 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Lightning extends PApplet {
+
 lightning tom;
-void setup()
+public void setup()
 {
   size(800,500); 
   frameRate(60);
 }
-void draw()
+public void draw()
 {
   fill(0, 25);
   rect(-25, -25, 850, 550);
 }
-void mousePressed()
+public void mousePressed()
 {
   for(int x = 0; x <= (int)(Math.random()*16); x += 1)
   {
@@ -25,7 +41,7 @@ class lightning
   	lightX = x;
   	lightY = y;
   }
-  void zap()
+  public void zap()
   {
   	int strikeX;
   	int strikeY;
@@ -45,5 +61,14 @@ class lightning
   		lightX = strikeX;
   		lightY = strikeY;
   	}
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Lightning" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
